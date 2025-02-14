26ec91280786a3bae6d6ded3e6f48142a1948a43
Vijitha P <vijitha.p@experionglobal.local>
2025-02-14 11:47:30 +0530

    features/home

--- my-app/src/Features/Header/intex.tsx
+++ my-app/src/Features/Header/intex.tsx
@@ -2,9 +2,9 @@ import { ReactElement } from "react";
 
 export function Header(): ReactElement {
   return (
-    <div className="h-16 flex items-center justify-between">
-      <h1 className="text-red-500 font-semibold text-lg pl-20">Movie Time</h1>
-      <button className="bg-red-500 px-3 py-1 rounded-md text-white mr-20">
+    <div className="h-14 flex items-center justify-between shadow-xl">
+      <h1 className="text-red-500 font-extrabold text-lg pl-14">Movie Time</h1>
+      <button className="bg-red-500 px-3 py-1 rounded-md text-white mr-14">
         Sign in
       </button>
     </div>
